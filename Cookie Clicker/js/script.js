148504a9f657c92755771069af93bc0ef6195856
João Pedro Morangoni <124791845+joaomorangoni@users.noreply.github.com>
2024-02-22 12:40:46 -0300

    Add files via upload

---
+++ Cookie Clicker/js/script.js
@@ -0,0 +1,13 @@
+var Cookie = document.getElementById('cookie')
+var quant = 0
+var name = prompt('Digite o seu nome: ')
+
+
+function cookies(){
+    quant += 1;
+    quantia.innerHTML = quant + ' Cookies!';
+}
+
+nome.innerHTML = 'Fazenda de ' + name;
+console.log(quant)
+Cookie.addEventListener('click',cookies)
